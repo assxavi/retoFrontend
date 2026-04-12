4d3b23a326519c9f833049461276b8158175c47d
Hector <hectorgomendez@gmail.com>
2026-04-12 20:15:33 +0200

    feat: instalado bootsrap ya que se requiere para el reto

--- src/app/shared/components/barra-lateral/barra_lateral.ts
+++ src/app/shared/components/barra-lateral/barra_lateral.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+export interface SidebarLink {
+  label: string;
+  route: string;
+  icon?: string; // clase de FontAwesome, ej: 'fa-calendar'
+}
 
 @Component({
   selector: 'app-barra-lateral',
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './barra_lateral.html',
   styleUrl: './barra_lateral.scss',
 })
-export class BarraLateral {}
+export class BarraLateral {
+  @Input() title: string = '';
+  @Input() links: SidebarLink[] = [];
+}

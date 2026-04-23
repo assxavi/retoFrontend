fcea49b5a5b1fd03ce0b05d1b5dbec52b8322e7c
Alvaro Postigo <alvaro.postigo440@comunidadunir.net>
2026-04-23 21:36:04 +0200

    feat: update services to use environment variables for API URLs

--- src/app/core/services/tipo.service.ts
+++ src/app/core/services/tipo.service.ts
@@ -2,13 +2,14 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tipo } from '../../models/tipo.model';
+import { environment } from '../../../enviroment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TipoService {
   private http = inject(HttpClient);
-  private apiUrl = 'http://localhost:8080/tipos';
+  private apiUrl = `${environment.apiUrl}/eventos`;
 
   findAll(): Observable<Tipo[]> {
     return this.http.get<Tipo[]>(`${this.apiUrl}/listado`);
